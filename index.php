c96883e7c9f2abe9231aaddd277113442b872107
root <root@Silo.lib.wayne.edu>
2014-01-07 13:17:51 -0500

    major changes, word frequency added

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
 
     <title>TextSuite - Wayne State</title>
     <!-- jQuery must be included before Annotator -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>   
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>   
+
+    <!--Google Charts-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script> 
          
     <!-- The main Annotator script -->    
      <script src="inc/annotator/annotator-full.min.js"></script>
@@ -21,7 +24,9 @@
     <link href="css/textAnalysis.css" rel="stylesheet">    
 
     <!--JS loads-->          
-    <script src="js/textAnalysis.js"></script>        
+    <script src="js/textAnalysis.js"></script>      
+
+     
 
     
 
@@ -48,8 +53,8 @@
               <button>text</button>
             </div>
             <div id="right_pane_header">
-              <button>annotation</button>
-              <button>analysis</button>              
+              <button onclick="fireAnno(); return false;">annotation</button>
+              <button onclick="fireAnalysis(); return false;">analysis</button>              
             </div>
           </div>
 
@@ -57,15 +62,37 @@
         </div>
         <!-- Keep all page content within the page-content inset div! -->
         <div class="page-content">
-          <div class="row">
-            <div id="left_pane">
-              <div id="image_container">
-                <img src=""/>
+          <div id="annotation_layer">
+            <div class="row">
+              <div class="left_pane">
+                <div id="image_container">
+                  <img src=""/>
+                </div>
               </div>
+              <div class="right_pane">
+                <div id="right_pane_HTML"></div>
+              </div>            
             </div>
-            <div id="right_pane">
-              <div id="right_pane_HTML"></div>
-            </div>            
+          </div>
+          <div id="analysis_layer">
+            <div class="row">
+              <h2>Textual Analysis</h2>
+              <div class="left_pane">
+                <form onsubmit="stringSearch(); return false;">
+                  <label>String to search for:</label>
+                  <input id="search_string" type='textbox'/>
+
+                  <input type="submit"/>
+                </form>
+              </div>
+              <div class="right_pane">
+                <div id="stringSearch_lineGraph"></div>
+                <script type="text/javascript">
+                  paintWordFreq();
+                </script>
+              </div>
+              
+            </div>
           </div>
         </div>
       </div>
--- js/textAnalysis.js
+++ js/textAnalysis.js
@@ -1,5 +1,9 @@
 // Globals
 var textMeta = new Object();
+var analysisBlob = new Object();
+var Charts = new Object();
+Charts.Handles = new Object();
+Charts.Options = new Object();
 
 // on page launch
 function launch(PID, pagenum){
@@ -38,6 +42,9 @@ function launch(PID, pagenum){
 }
 
 function updatePage(pagenum){
+	// hide analysis, fade in annotations
+	$("#analysis_layer").hide();
+	$("#annotation_layer").fadeIn();
 	// swap out image
 	insertImage(pagenum);
 	// swap out text
@@ -50,8 +57,8 @@ function annoLaunch(pagenum,type){
 
 	if (type == 'launch'){
 		jQuery(function ($) {	    
-		    $('#right_pane').annotator();
-		    $('#right_pane').annotator('addPlugin', 'Store', {
+		    $('.right_pane').annotator();
+		    $('.right_pane').annotator('addPlugin', 'Store', {
 		    	prefix: 'http://silo.lib.wayne.edu/annotations',
 		    	annotationData: {
 		    		'uri':uri
@@ -67,10 +74,10 @@ function annoLaunch(pagenum,type){
 	if (type == 'update'){		
 		jQuery(function ($) {
 			// clear annotations
-			$('#right_pane').data('annotator').plugins['Store'].annotations = [];	    
-		    $('#right_pane').data('annotator').plugins['Store'].options.loadFromSearch.uri = uri;
-		    $('#right_pane').data('annotator').plugins['Store'].options.annotationData.uri = uri;		    
-		    $('#right_pane').data('annotator').plugins['Store'].loadAnnotationsFromSearch({		    	
+			$('.right_pane').data('annotator').plugins['Store'].annotations = [];	    
+		    $('.right_pane').data('annotator').plugins['Store'].options.loadFromSearch.uri = uri;
+		    $('.right_pane').data('annotator').plugins['Store'].options.annotationData.uri = uri;		    
+		    $('.right_pane').data('annotator').plugins['Store'].loadAnnotationsFromSearch({		    	
 		    	'uri':uri
 		    });		    
 		});
@@ -105,7 +112,169 @@ function insertImage(pagenum){
 	$("#image_container img").attr('src','http://silo.lib.wayne.edu/fedora/objects/'+textMeta.PIDsuffix+':images/datastreams/IMAGE_'+pagenum+'/content');
 }
 
-// listeners
+// fire Annotations
+function fireAnno(){
+	$("#analysis_layer").hide();
+	$("#annotation_layer").fadeIn();
+}
+
+// fire Analysis
+function fireAnalysis(){	
+	$("#annotation_layer").hide();
+	$("#analysis_layer").fadeIn();
+	
+
+	// load analysis template
+}
+
+
+
+// Text Analysis
+//////////////////////////////////////////////////////////////////////
+
+function stringSearch(){
+	// http://silo.lib.wayne.edu/WSUAPI-dev/?functions[]=eTextSearch&ItemID=yellowwallpaper&search_term=%22chintz%22
+
+	var solrParams = new Object();
+	solrParams.q = $("#search_string").val();
+	solrParams['fq[]'] = [
+		"ItemID:"+textMeta.structMeta.XMLtoJSON.ebook_item.item_ID
+		];
+	solrParams.rows = 1000;
+	solrParams.sort = "page_num asc";
+	solrParams.hl = "true";
+	solrParams['hl.fl'] = "OCR_text";
+	solrParams['hl.snippets'] = 1000;
+	solrParams['hl.fragmenter'] = 'gap';
+	solrParams['hl.fragsize'] = 70;
+
+	//pass solr parameters os stringify-ed JSON, accepted by Python API as dicitonary
+	solrParamsString = JSON.stringify(solrParams);	
+	// Calls API functions	
+	var APIcallURL = "http://silo.lib.wayne.edu/WSUAPI-dev?functions[]=eTextSearch&solrParams="+solrParamsString;
+
+	$.ajax({          
+	  url: APIcallURL,      
+	  dataType: 'json',	  	    
+	  success: callSuccess,
+	  error: callError
+	});
+
+	function callSuccess(response){
+		console.log(response);
+		analysisBlob.stringSearch = response.eTextSearch;
+		paintWordFreq_Update();
+	}
+	function callError(response){
+		console.log(response);
+	}
+
+}
+
+// function to create Google Chart of StringPerPage
+function paintWordFreq(){
+
+	google.load("visualization", "1", {packages:["corechart"]});	
+	google.setOnLoadCallback(drawChart);	
+	function drawChart() {    
+		
+
+		var data = google.visualization.arrayToDataTable([
+          ['Page', 'Count'],
+          ['',  0]
+        ]);
+
+		Charts.Options.stringSearch_lineGraph = {
+		  title: "Word Frequency per Page",
+		  titleTextStyle: {color: '#333'},
+		  vAxis: {title: 'Count',  
+		          titleTextStyle: {color: '#333', italic: 'false', bold: 'true'},
+		          textStyle: {fontSize: '12'},
+		          minValue:0,
+		          format:"#"
+		      },
+		  hAxis: {title: 'Page',  
+		          titleTextStyle: {color: '#333', italic: 'false', bold: 'true'}, 
+		          textStyle: {fontSize: '12'},
+		          slantedText: 'true',
+		          slantedTextAngle: '65',
+		          gridlines: {color: '#aaa', count: '5'}, 
+		          minorGridlines: {color: '#eee', count: '4'}
+		      },
+		  pointSize: '3',
+		  fontSize: '14',
+		  fontName: 'Arial',
+		  height: 500,
+		  legend: 'none',
+		  // chartArea: {'width': '90%', 'height': '60%', top: '60', left: '90'},
+		  // curveType: 'function'
+		};
+
+		Charts.Handles.stringSearch_lineGraph = new google.visualization.LineChart(document.getElementById('stringSearch_lineGraph'));
+		Charts.Handles.stringSearch_lineGraph.draw(data, Charts.Options.stringSearch_lineGraph);
+	}
+}
+
+function paintWordFreq_Update(){
+	// Initiate data table    
+	var data = new google.visualization.DataTable();
+	// Set columns
+	data.addColumn('number','page');
+	data.addColumn('number','count');
+
+	// create object to push to data
+	var JSONobject = [];
+
+	// create data points for each page
+	for (var i=0; i < textMeta.structMeta.XMLtoJSON.ebook_item.dimensions.leafs; i++) {
+		// page name - textMeta.structMeta.XMLtoJSON.ebook_item.item_ID + i
+		var dataRow = new Object();
+		// dataRow['page'] = textMeta.structMeta.XMLtoJSON.ebook_item.item_ID+"_OCR_HTML_"+(i+1);
+		dataRow['page'] = (i+1);
+		dataRow['count'] = 0;
+		JSONobject.push(dataRow);
+	}
+
+	console.log("Pre highlights addition:",JSONobject);	
+	
+	// iterate through highlights, push to JSONobject that has all pages from text
+	var snippet_keys = Object.keys(analysisBlob.stringSearch.highlighting);
+	snippet_keys.alphanumSort();
+	for (var i = 0; i < snippet_keys.length; i++) {
+		var pageSnippets = analysisBlob.stringSearch.highlighting[snippet_keys[i]];
+		var pageNum = snippet_keys[i].split("_OCR_HTML_")[1];
+		JSONobject[(pageNum - 1)]['count'] = pageSnippets.OCR_text.length;	
+	}
+
+	// // iterate through pages, creates JSONobject that is ONLY highlights
+	// var snippet_keys = Object.keys(analysisBlob.stringSearch.highlighting);
+	// snippet_keys.alphanumSort();
+	// for (var i = 0; i < snippet_keys.length; i++) {
+	// 	var pageSnippets = analysisBlob.stringSearch.highlighting[snippet_keys[i]];
+	// 	var dataRow = new Object();
+	// 	dataRow['page'] = snippet_keys[i];
+	// 	dataRow['count'] = pageSnippets.OCR_text.length;
+	// 	// dataRow[snippet_keys[i]] = pageSnippets.OCR_text.length;
+	// 	JSONobject.push(dataRow);
+	// }
+
+	console.log("After insertions",JSONobject);
+	 
+	// Add rows to data object
+	for (var i=0; i<JSONobject.length; i++){
+	  data.addRow([ 
+	    JSONobject[i]['page'],JSONobject[i]['count']
+      ]);
+	}	
+	Charts.Handles.stringSearch_lineGraph.draw(data, Charts.Options.stringSearch_lineGraph);
+}
+
+
+
+
+
+
+// LISTENERS
 
 // resizing
 $(window).resize(function() {
@@ -126,8 +295,41 @@ function resizePageThumbs(){
 }
 
 
+// UTILITIES
 
+Array.prototype.alphanumSort = function(caseInsensitive) {
+  for (var z = 0, t; t = this[z]; z++) {
+    this[z] = []; var x = 0, y = -1, n = 0, i, j;
 
+    while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+      var m = (i == 46 || (i >=48 && i <= 57));
+      if (m !== n) {
+        this[z][++y] = "";
+        n = m;
+      }
+      this[z][y] += j;
+    }
+  }
+
+  this.sort(function(a, b) {
+    for (var x = 0, aa, bb; (aa = a[x]) && (bb = b[x]); x++) {
+      if (caseInsensitive) {
+        aa = aa.toLowerCase();
+        bb = bb.toLowerCase();
+      }
+      if (aa !== bb) {
+        var c = Number(aa), d = Number(bb);
+        if (c == aa && d == bb) {
+          return c - d;
+        } else return (aa > bb) ? 1 : -1;
+      }
+    }
+    return a.length - b.length;
+  });
+
+  for (var z = 0; z < this.length; z++)
+    this[z] = this[z].join("");
+}
 
 
 
