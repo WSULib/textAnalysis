2d159478150e9643dfab028f25bd07ba89b9bcd1
Cole Hudson <root@Silo.lib.wayne.edu>
2014-02-04 21:01:40 -0500

    small fixes to login

--- js/userData.js
+++ js/userData.js
@@ -5,10 +5,10 @@ var userData = new Object();
 
 // check WSUDOR cookie
 $(document).ready(function(){
-	console.log("Checking WSUDOR cookie");
+	// console.log("Checking WSUDOR cookie");
 	if (typeof $.cookie("WSUDOR") != 'undefined'){
 		WSUDORcookie = $.parseJSON($.cookie("WSUDOR"));
-		console.log(WSUDORcookie);      
+		// console.log(WSUDORcookie);      
 		userData.username_WSUDOR = WSUDORcookie.username_WSUDOR;
 		userData.loggedIn_WSUDOR = WSUDORcookie.loggedIn_WSUDOR;
 		userData.displayName = WSUDORcookie.displayName;
@@ -25,23 +25,23 @@ $(document).ready(function(){
 			postData.username = userData.username_WSUDOR;
 			postData.clientHash = userData.clientHash;
 			var APIaddURL = "http://silo.lib.wayne.edu/WSUAPI?functions[]=cookieAuth";
-			console.log(APIaddURL);
+			// console.log(APIaddURL);
 			$.ajax({          
 				url: APIaddURL,
 				type: "POST",
 				data: postData,      
 				dataType: 'json',
 				success: function(response){
-				  console.log("cookieAuth response:");
-				  console.log(response);
+				  // console.log("cookieAuth response:");
+				  // console.log(response);
 				  if (response.cookieAuth.hashMatch == false || typeof(response.cookieAuth.hashMatch) == "undefined"){
-				  	console.log("Attack!  Attack!  Deleting cookie and refreshing.");
+				  	// console.log("Attack!  Attack!  Deleting cookie and refreshing.");
 				  	logoutUser();				  	
 				  }
 				  
 				},
 				error: function(response){
-				  console.log(response);
+				  // console.log(response);
 				  alert("Error.");
 				}
 			});			
